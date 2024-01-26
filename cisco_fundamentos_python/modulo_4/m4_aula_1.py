5912862bcc551dedb0975061921e242982e3d74f
Dualsgo <73504705+dualsgo@users.noreply.github.com>
2024-01-25 22:02:57 -0300

    PE1 - Introdução funções

---
+++ cisco_fundamentos_python/modulo_4/m4_aula_1.py
@@ -0,0 +1,174 @@
+# 4.1 Seção 1 - Funções
+
+# 4.1.1 Por que precisamos de funções?
+# Até agora, discutimos funções principalmente como ferramentas para simplificar tarefas. Agora, vamos abordar a criação e uso de suas próprias funções.
+
+# Às vezes, trechos de código são repetidos ou clonados no programa, o que pode ser problemático se houver erros ou modificações necessárias. A primeira condição para escrever funções é quando um fragmento de código aparece em mais de um lugar; nesse caso, isole-o em uma função chamada nos pontos onde o código original estava.
+
+# Se o código se tornar tão extenso que a leitura e a manutenção se tornarem desafiadoras, a segunda condição é aplicada: divida-o em partes menores e implemente cada uma como uma função separada. Esse processo, conhecido como decomposição, simplifica o trabalho, permitindo que cada parte seja codificada e testada independentemente.
+
+# Em resumo, escrever suas próprias funções é uma prática eficaz para evitar repetição de código, melhorar a legibilidade e facilitar a manutenção do programa.
+
+# 4.1.2 Decomposição
+# Quando um problema de programação é tão extenso que requer a colaboração de uma equipe, é essencial dividir o trabalho entre os desenvolvedores para garantir eficiência. Isso não apenas facilita a cooperação, mas também distribui a responsabilidade entre os membros da equipe.
+
+# Cada desenvolvedor é encarregado de escrever um conjunto específico de funções bem definidas. Essas funções, quando combinadas nos módulos (abordaremos isso mais tarde), formarão o produto final. Portanto, ao dividir o trabalho entre programadores, é crucial decompor o problema de maneira que o produto possa ser implementado como um conjunto de funções independentes, reunidas em diferentes módulos.
+
+# 4.1.3 De onde vêm as funções?
+# Em geral, as funções vêm de pelo menos três lugares:
+
+# do próprio Python - várias funções (como print()) são parte integrante do Python, e estão sempre disponíveis sem nenhum esforço adicional em nome do programador; chamamos essas funções de funções internas;
+
+# dos módulos pré-instalados do Python - muitas funções, muito úteis, mas usadas muito menos frequentemente do que as integradas, estão disponíveis em vários módulos instalados junto com o Python; o uso dessas funções requer algumas etapas adicionais do programador para torná-las totalmente acessíveis (falaremos sobre isso daqui a pouco);
+
+# diretamente do seu código - você pode escrever suas próprias funções, colocá-las dentro do código e usá-las livremente;
+
+# há uma outra possibilidade, mas ela está conectada às classes, então vamos omitir isso por enquanto.
+
+# 4.1.4 Sua primeira função
+# Vamos começar com uma função muito simples. Vamos escrever uma função que exibe uma mensagem na tela e nada mais. A mensagem será sempre a mesma, mas você poderá usá-la em qualquer lugar do seu código, quantas vezes quiser. É bem simples, mas queremos que seja apenas um exemplo de transformação de uma parte repetida de um código em uma função.
+
+# Se quisermos solicitar um valor do usuário, podemos usar a função input().
+print("Entre um valor: ")
+a = int(input())
+
+print("Entre um valor: ")
+b = int(input())
+
+print("Entre um valor: ")
+c = int(input())
+
+# Note que as mensagens enviadas para o console pela função print() são sempre as mesmas. Claro, não há nada de ruim nesse código, mas tente imaginar o que você teria que fazer se seu chefe pedisse para você mudar a mensagem para torná-la mais educada, por exemplo, para começar com a frase "Por favor".
+# Com pouco código, não seria um problema, mas e se houvesse centenas de mensagens como essa?
+# Parece que você precisaria gastar algum tempo alterando todas as ocorrências da mensagem (você usaria uma área de transferência, é claro, mas isso não tornaria sua vida muito mais fácil). É óbvio que você provavelmente cometeria alguns erros durante o processo de alteração, e você (e seu chefe) ficaria um pouco frustrado.
+
+# É possível separar uma parte que pode ser repetida, nomeá-la e torná-la reutilizável? (Isso significaria que uma alteração feita uma vez em um só lugar seria propagada para todos os lugares onde é usada. Obviamente, um código como esse só deve funcionar quando for iniciado explicitamente).
+
+# Sim, é possível. É para isso que servem as funções.
+
+# Você precisa defini-lo. A palavra 'define' é significativa aqui.
+# É assim que a definição mais simples de função se parece:
+'''
+def function_name():
+    function_body
+'''
+
+# Começa sempre com a palavra-chave def (de define)
+# Depois de def passa o nome da função (as regras para nomear funções são exatamente as mesmas que para nomear variáveis)
+# depois do nome da função, há um lugar para um par de parênteses (eles não contêm nada aqui, mas isso vai mudar em breve)
+# a linha deve ser terminada com dois pontos;
+# a linha logo após def inicia o corpo da função - um par (pelo menos um) de instruções necessariamente aninhadas, que serão executadas sempre que a função for chamada; nota: a função termina onde o aninhamento termina, então você precisa ter cuidado.
+# as regras de aninhamento são as mesmas que para as instruções if, for, while, etc. - você pode usar espaços ou tabulações, mas não misture os dois.
+
+# Estamos prontos para definir nossa função de prompt. Vamos dar o nome de message - aqui está:
+
+def message():
+    print("Entre um valor: ") # o corpo da função - observe a indentação
+# A função é extremamente simples, mas totalmente utilizável. Atribuímos o nome de message, mas você pode nomear de acordo com o seu gosto. Vamos usá-lo.
+# print("Entre um valor: ") será o efeito colateral da função - não é o que a função retorna, mas o que ela faz. A função não retorna nada, mas faz algo. É por isso que a chamamos de efeito colateral.
+
+# Nosso código contém a definição de função agora:
+
+print("Começamos aqui.")
+print("terminamos aqui.")
+# Nota: não usamos a função - não há nenhuma chamada dentro da função.
+
+# Ao executá-lo, você vê a seguinte saída:
+'''
+Começamos aqui.
+terminamos aqui.
+'''
+
+# Isso significa que o Python lê as definições da função e as lembra, mas não inicia nenhuma sem a sua permissão. Ou seja, precisamos dizer ao Python para executar a função. Isso é chamado de invocação da função.
+# Nós modificamos o código agora - inserimos a chamada da função entre as mensagens de início e fim:
+
+print("Começamos aqui.")
+message() # chamada da função
+print("terminamos aqui.")
+# A saída está diferente agora:
+"""
+Começamos aqui.
+Entre um valor: # mensagem da função
+terminamos aqui.
+"""
+# O bloco de código que foi definido como o corpo da função foi executado. O Python lembra o local onde a função foi chamada e retorna a esse local quando a função termina.
+# A função não retorna nada, mas faz algo. É por isso que a chamamos de efeito colateral.
+
+# 4.1.5 Como funções funcionam
+# quando você invoca uma função, o Python se lembra do local onde aconteceu e salta para a função invocada;
+# o corpo da função é então executado;
+# alcançar o final da função força o Python a retornar ao local imediatamente após o ponto de chamada.
+
+# Há duas coisas muito importantes. Aqui está a primeira:
+# Você não deve invocar uma função que não é conhecida no momento da invocação. Lembre - se o Python lê seu código de cima para baixo. Ela não vai olhar para frente para encontrar uma função que você esqueceu de colocar no lugar certo ("certo" significa "antes da invocação".)
+
+"""
+# Inserimos um erro neste código - Vê a diferença?
+Movemos a função para o final do código. O Python consegue encontrá-lo quando a execução atinge a invocação? Não, não é. A mensagem de erro será:
+
+NameError: name 'message' is not defined
+
+# Não tente forçar o Python a procurar funções que não foram disponibilizadas no momento certo.
+
+
+
+print("Começamos aqui.")
+message()
+print("terminamos aqui.") 
+
+
+def message():
+    print("Entre um valor: ")
+"""
+# Segundo: Você não deve ter uma função e uma variável com o mesmo nome.
+# Dependendo da ordem em que você os define o código até pode funcionar, mas não é uma boa prática de programação. Você pode se confundir e o Python também.
+# É boa prática usar um certo padrão para nomear funções com verbos que tornam sua função mais descritiva assim como deixar claro o que uma variável guarda.
+
+# Vamos retornar ao nosso exemplo principal e empregar a função para o trabalho certo, como aqui:
+
+def message():
+    print("Entre um valor: ")
+
+message() # chamada da função
+a = int(input())
+message() # chamada da função
+b = int(input())
+message() # chamada da função
+c = int(input())
+
+# O código funciona como antes, mas agora é muito mais fácil de ler e entender. Você pode ver que a função é chamada três vezes, mas o código que ela executa é escrito apenas uma vez. Isso é muito bom. Poderíamos também adicionar argumentos à função para torná-la ainda mais flexível, definindo um texto diferente para cada chamada.
+# Claro que essa tarefa é simples demais para exigi-la de uma função, mas é apenas um exemplo. Você pode imaginar como isso pode ser útil em um programa maior.
+
+# # 4.1.6 RESUMO DA SEÇÃO
+# 1. Uma função é um bloco de código que executa uma tarefa específica quando a função é chamada (chamada). Você pode usar funções para tornar seu código reutilizável, melhor organizado e mais legível. As funções podem ter parâmetros e valores de retorno.
+
+# 2. Há pelo menos quatro tipos básicos de funções no Python:
+
+# - incorporadas que são parte integrante do Python (como a função de print()). Você pode ver uma lista completa das funções integradas do Python em https://docs.python.org/3/library/functions.html.
+# - os que vêm de módulos pré-instalados (você aprenderá sobre eles no curso Fundamentos do Python 2)
+# - funções definidas pelo usuário que são escritas pelos usuários para os usuários - você pode escrever suas próprias funções e usá-las livremente no código,
+# - as funções lambda (você aprenderá sobre elas no curso Fundamentos do Python 2.)
+
+# 3. Você pode definir sua própria função usando a palavra-chave def e a seguinte sintaxe:
+
+# def your_function(optional_parameters):
+    # o corpo da função
+
+# Você pode definir uma função que não aceita argumentos, por exemplo:
+
+def message():  # definindo uma função
+    print("Olá")  # o corpo da função
+
+message()  # chamando a função
+
+# Você pode definir uma função que aceita argumentos, assim como a função de um parâmetro abaixo:
+
+def hello(name):  # definindo uma função
+    print("Olá,", name)  # o corpo da função
+
+name = input("Entre um valor: ")
+
+hello(name)  # chamando a função
+
+# Falaremos sobre funções parametrizadas na próxima seção. Não se preocupe.
+
---
+++ cisco_fundamentos_python/modulo_4/m4_aula_2.py
@@ -0,0 +1,247 @@
+# 4.2.1 Funções parametrizadas
+# O poder total da função se revela quando pode ser equipado com uma interface capaz de aceitar dados fornecidos pelo invocador. Esses dados podem modificar o comportamento da função, tornando-a mais flexível e adaptável às condições variáveis.
+
+# Um parâmetro é na verdade uma variável, mas há dois fatores importantes que tornam os parâmetros diferentes e especiais:
+
+# parâmetros existem apenas dentro de funções nas quais eles foram definidos, e o único lugar onde o parâmetro pode ser definido é um espaço entre um par de parênteses na declaração def;
+# a atribuição de um valor ao parâmetro é feita no momento da chamada da função, especificando o argumento correspondente.
+
+# def function(parameter):
+    ### corpo da função
+
+# funciton(argument)
+
+# Não se esqueça:
+# parâmetros vivem em funções internas (este é o ambiente natural)
+# argumentos existem fora das funções e são portadores de valores passados para os parâmetros correspondentes.
+
+# Há uma fronteira clara e não ambígua entre esses dois mundos.
+
+# Vamos agora melhorar o corpo da função:
+# A definição especifica que nossa função opera em apenas um parâmetro chamado number. Você pode usá-lo como uma variável comum, mas apenas dentro da função - não é visível em nenhum outro lugar.
+
+def message(number):
+    print("Digite um número:", number)
+
+# Um valor para o parâmetro chegará do ambiente da função. Lembre-se: especificar um ou mais parâmetros na definição de uma função também é um requisito, e você precisa preenchê-lo durante a chamada. Você deve fornecer quantos argumentos houver parâmetros definidos. Não fazer isso causará um erro.
+"""
+def message(number): # Função definida com um parâmetro
+  print("Digite um número:", number)
+
+message() # Função chamada sem argumento
+
+# TypeError: message() missing 1 required positional argument: 'number'
+"""
+def message(number):
+    print("Digite um número:", number)
+
+message(1) # Digite um número: 1
+message(598) # Digite um número: 598
+
+# Você consegue ver como isso funciona? O valor do argumento usado durante a invocação (1) foi passado para a função, definindo o valor inicial do parâmetro chamado number.
+
+# Temos que torná-lo sensível a uma circunstância importante. É legal e possível ter uma variável com o mesmo parâmetro de função.
+
+# O trecho ilustra o fenômeno:
+def message(number):
+    print("Digite um número:", number)
+
+number = 1234
+message(1) # Digite um número: 1
+print(number) # 1234
+
+# Uma situação como essa ativa um mecanismo chamado shadowing:
+# o parâmetro da função é obscurecido pela variável global com o mesmo nome.
+# O parâmetro ainda existe, mas não é visível dentro da função.
+# Evitamos esse fenômeno, dando nomes diferentes às variáveis e parâmetros.
+
+# Uma função pode ter quantos parâmetros você quiser, mas quanto mais parâmetros tiver, mais difícil será memorizar suas funções e propósitos.
+
+# Vamos modificar a função - ela tem dois parâmetros agora:
+def message(what, number):
+    print("Entrar", what, "número", number)
+    
+# Isso também significa que a invocação da função exigirá dois argumentos.
+message("um", 1)
+
+# Os tipos de parâmetros e argumentos não são verificados pelo Python. Você pode passar qualquer valor para qualquer parâmetro. O Python não se importa com isso. É sua responsabilidade garantir que os valores sejam adequados para o propósito da função.
+
+# 4.2.2 Passagem de parâmetros posicionais
+# Uma técnica que atribui o i-ésimo (primeiro, segundo e assim por diante) argumento para o i-ésimo (primeiro, segundo, etc.) parâmetro de função é chamada de passagem de parâmetro posicional, enquanto argumentos passados dessa maneira são chamados de argumentos posicional.
+
+# Você já usou, mas o Python pode oferecer muito mais. Vamos falar sobre isso agora.
+def my_function(a, b, c): # a, b, c são parâmetros, exatamente nessa ordem
+    print(a, b, c)
+# Isso significa que os argumentos passados para a função devem ser colocados na mesma ordem.
+my_function(1, 2, 3)
+
+# Nota: a passagem de parâmetro posicional é usada intuitivamente por pessoas em várias ocasiões sociais. Por exemplo, pode-se aceitar que, quando nos apresentamos, mencionamos nossos nomes antes de nosso sobrenome, por exemplo, "Meu nome é John Doe".
+
+# Vamos implementar esse costume social em Python. A função a seguir será responsável por apresentar alguém:
+def introduction(first_name, last_name):
+    print("Olá meu nome é", first_name, last_name)
+
+introduction("Luke", "Skywalker") # Olá meu nome é Luke Skywalker
+introduction("Jesse", "Quick") # Olá meu nome é Jesse Quick
+introduction("Clark", "Kent") # Olá meu nome é Clark Kent
+
+# A propósito, os húngaros fazem isso em ordem inversa.
+def introduction(first_name, last_name):
+    print("Olá meu nome é", first_name, last_name)
+
+introduction("Skywalker", "Luke") # Olá meu nome é Skywalker Luke
+introduction("Quick", "Jesse") # Olá meu nome é Quick Jesse
+introduction("Kent", "Clark") # Olá meu nome é Kent Clark
+
+# 4.2.3 Passagem de parametro por palavra-chave
+# O Python oferece outra convenção para a passagem de argumentos, em que o significado do argumento é determinado por seu nome, e não por sua posição - é chamado de passagem de argumento de palavra-chave.
+def introduction(first_name, last_name):
+    print("Olá meu nome é", first_name, last_name)
+
+introduction(first_name = "James", last_name = "Bond") # Olá meu nome é James Bond
+introduction(last_name = "Skywalker", first_name = "Luke") # Olá meu nome é Luke Skywalker
+
+# O conceito é claro - os valores passados para os parâmetros são precedidos pelos nomes dos parâmetros de destino, seguidos pelo sinal =.
+# A posição não importa aqui - o valor de cada argumento sabe seu destino com base no nome usado.
+# Obviamente, você não deve usar um nome de parâmetro inexistente. O Python não aceitará isso e gerará um erro. TypeError: introduction() got an unexpected keyword argument 'surname'
+
+# 4.2.4 Mistura de argumentos posicionais e de palavras-chave
+# Você pode combinar os dois estilos, se quiser - há apenas uma regra inquebrável: 
+# você precisa colocar argumentos posicionais antes dos argumentos das palavras-chave. Se você pensar por um momento, certamente entenderá o porquê.
+
+# Seu objetivo é avaliar e apresentar a soma de todos os seus argumentos.
+def adding(a, b, c):
+    print(a, "+", b, "+", c, "=", a + b + c)
+
+# A função, quando chamada da seguinte maneira:
+adding(1, 2, 3) # 1 + 2 + 3 = 6
+# Obviamente, você pode substituir essa chamada por uma variante de palavra-chave, como esta:
+# adding(c = 1, a = 2, b = 3)
+
+# Vamos tentar combinar os dois estilos agora.
+# adding(3, c = 1, b = 2)
+# Vamos analisar:
+# o argumento (3) para o parâmetro a é passado usando a maneira posicional;
+# os argumentos para c e b são especificados como palavras-chave.
+# essa chamada é válida e produzirá a mesma saída que as duas chamadas anteriores.
+
+# Porém, se você tentar fazer isso:
+# adding(3, a = 1, b = 2)
+# TypeError: adding() got multiple values for argument 'a'
+
+# Você receberá um erro de tempo de execução. O Python não pode aceitar isso, pois não pode decidir qual valor deve ser atribuído ao parâmetro a - o valor posicional (3) ou o valor da palavra-chave (1).
+# Tenha cuidado e cuidado com os erros. Se você tentar passar mais de um valor para um argumento, tudo que obterá será um erro de tempo de execução.
+
+# 4.2.5 Funções parametrizadas – mais detalhes
+# Às vezes, os valores de um determinado parâmetro são usados com mais frequência do que outros. Esses argumentos podem ter seus valores padrão (predefinidos) levados em consideração quando seus argumentos correspondentes foram omitidos. Isso é chamado de parâmetro padrão. Também é usado para evitar erros de tempo de execução.
+
+# Eles dizem que o sobrenome mais popular em inglês é Smith. Vamos tentar levar isso em conta.
+def introduction(first_name, last_name="Smith"):
+    print("Olá meu nome é", first_name, last_name)
+# Você só precisa estender o nome do parâmetro com o sinal =, seguido pelo valor padrão.
+
+introduction("James", "Doe") # Olá meu nome é James Doe
+introduction("James") # Olá meu nome é James Smith - o valor padrão é usado aqui pois só foi passado um argumento
+introduction(first_name="William") # Olá meu nome é William Smith 
+
+# Você pode usar argumentos posicionais e de palavras-chave em uma única chamada de função, mas você deve ter cuidado com a ordem dos argumentos posicionais. Eles devem vir primeiro.
+
+# Você pode ir além se for útil. Ambos os parâmetros têm seus valores padrão agora, veja o código abaixo:
+def introduction(first_name="John", last_name="Smith"):
+    print("Olá meu nome é", first_name, last_name)
+
+# Isso torna a seguinte chamada absolutamente válida:
+introduction() # Olá meu nome é John Smith - nenhum argumento foi passado para a função e os valores padrão foram usados
+
+# Os valores padrão são usados quando nenhum argumento é passado para o parâmetro correspondente. Diferente de caso você não defina valores padrão e não passe argumentos, você receberá um erro de tempo de execução.
+
+# # 4.2.6 RESUMO DA SEÇÃO
+# 1. Você pode passar informações para funções usando parâmetros. Suas funções podem ter quantos parâmetros forem necessários.
+
+# Um exemplo de função de um parâmetro:
+def hi(name):
+    print("Oi,", name)
+
+hi("Greg")
+
+# Um exemplo de função de dois parâmetros:
+def hi_all(name_1, name_2):
+    print("Oi,", name_2)
+    print("Oi,", name_1)
+
+hi_all("Sebastian", "Konrad")
+
+# Um exemplo de função de três parâmetros:
+def address(street, city, postal_code):
+    print("Seu endereço é:", street, "St.,", city, postal_code)
+
+s = input("Street: ")
+p_c = input("Código postal: ")
+c = input("Cidade: ")
+address(s, c, p_c)
+
+# 2. Você pode passar argumentos para uma função usando as seguintes técnicas:
+
+# - passagem de argumento posicional em que a ordem dos argumentos passou importa (Ex. 1)
+def subtra(a, b):
+    print(a - b)
+
+subtra(5, 2) # saídas: 3
+subtra(2, 5) # saídas: -3
+
+# - passagem de argumentos da palavra-chave (nomeada) na qual a ordem dos argumentos passados não importa (ex. 2)
+def subtra(a, b):
+    print(a - b)
+
+subtra(a=5, b=2) # saídas: 3
+subtra(b=2, a=5) # saídas: 3
+# - uma combinação de passagem de argumento de posição e de palavra-chave (por exemplo, 3.)
+def subtra(a, b):
+    print(a - b)
+
+subtra(5, b=2) # saídas: 3
+subtra(5, 2) # saídas: 3
+# subtra(b=2, a) # SyntaxError: non-default argument follows default argument
+
+# É importante lembrar que argumentos de posição não devem seguir argumentos de palavra-chave. 
+# É por isso que, se você tentar executar o seguinte trecho:
+
+# Exemplo inválido
+def subtra(a, b):
+    print(a - b)
+
+subtra(5, b=2) # saídas: 3
+# Syntax Error
+# subtra(a=5, 2) #  SyntaxError: positional argument follows keyword argument
+
+# 3. Você pode usar a técnica de passagem de argumento da palavra-chave para predefinir um valor para um determinado argumento:
+def name(first_name, last_name="Smith"):
+    print(first_name, last_name)
+
+name("Andy") # saídas: Andy Smith
+name("Betty", "Johnson") # saídas: Betty Johnson (the keyword argument replaced by "Johnson")
+
+
+# Teste
+"""
+def add_numbers(a, b=2, c):
+    print(a + b + c)
+
+add_numbers(a=1, c=3)
+# SyntaxError: non-default argument follows default argument
+"""
+# O código apresenta um erro de sintaxe. Quando você define uma função em Python e fornece um valor padrão para um argumento, todos os argumentos subsequentes também devem ter valores padrão. No entanto, na função add_numbers, você deu um valor padrão apenas para o segundo argumento (b=2), deixando o terceiro argumento (c) sem valor padrão. A correção seria dar um valor padrão também para o argumento c ou reorganizar a ordem dos argumentos na definição da função. Aqui estão duas maneiras corretas de fazer isso:
+"""
+def add_numbers(a, b=2, c=0):
+    print(a + b + c)
+
+add_numbers(a=1, c=3)
+
+"""
+
+
+
+
+
+
+
