6c3287b0898557efb13004c744e3b7d67593504f
mdbsilva <73504705+dualsgo@users.noreply.github.com>
2024-06-02 19:53:23 -0300

    represado

---
+++ Python/cisco_fundamentos_python/python_essentials_2/modulo_2/m2_aula3_lab1.py
@@ -0,0 +1,17 @@
+# 2.3.25 LAB Sua própria divisão. Você já sabe como funciona o split(). Agora queremos que você prove isso.
+
+# Sua tarefa é escrever sua própria função, que se comporta quase exatamente como o método split() original, ou seja:
+
+# ele deve aceitar exatamente um argumento – uma cadeia de caracteres;
+# ele deve retornar uma lista de palavras criadas a partir da cadeia de caracteres, dividida nos locais onde a cadeia contém espaços em branco;
+# se a cadeia de caracteres estiver vazia, a função deve retornar uma lista vazia;
+# seu nome deve ser mysplit()
+# Use o modelo no editor. Teste seu código cuidadosamente.
+
+"""
+['Para', 'ser', 'ou', 'não', 'para', 'ser', 'isso', 'é', 'o', 'pergunta']
+['Para', 'ser', 'ou', 'não', 'para', 'ser,isso', 'é', 'o', 'pergunta']
+[]
+['ABC']
+[]
+"""
